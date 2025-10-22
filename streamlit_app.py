5dcf86aae564c9d7bc9917f14197f76000eb382b
sophell <sophiemellery99@gmail.com>
2025-10-22 08:40:05 +0000

    Updating participant number logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit.components.v1 as components
 from PIL import Image
 import plotly.express as px
 import pandas as pd
+import math
 
 # Sidebar with useful links
 st.sidebar.title("Steps to Sweden")
@@ -16,10 +17,11 @@ st.sidebar.markdown("[Donation Log (Participants Only)](https://bit.ly/s2s-donat
 st.set_page_config(layout="centered")
 
 ####### UPDATE THESE VALUES #######
-today_date = '21 October 2025 12:55'
+today_date = '22 October 2025 08:39'
 current_distance = 1006  # km
 map_image = Image.open("Completed Oct-20.png")
 leaderboard_df = pd.read_csv("2025-10-20.csv")
+leaderboard_df = leaderboard_df[leaderboard_df['DISTANCE'] > 0]
 number_participants = len(leaderboard_df)
 
 milestones = {
@@ -37,11 +39,16 @@ milestones = {
     "Stockholm (2650km)": ""
     }
 
-end_date = pd.to_datetime("2025-11-04")
-start_date = pd.to_datetime("2025-10-14")
+end_date = pd.to_datetime("2025-11-04 23:59")
+start_date = pd.to_datetime("2025-10-14 00:00")
 
 if pd.to_datetime("today") > start_date:
-    days_remaining = (end_date - pd.to_datetime("today")).days
+    bst_date = pd.to_datetime("today").tz_localize("UTC").tz_convert("Europe/London").strftime("%d %B %Y %H:%M")
+    time_diff = end_date - pd.to_datetime(bst_date)
+    days_remaining = time_diff.days
+    hours_remaining = time_diff.seconds // 3600
+    days_remaining_exact = time_diff.total_seconds() / (24 * 3600)
+    total_days_left = math.ceil(days_remaining_exact)
 else:
     days_remaining = (end_date - start_date).days
 
@@ -79,7 +86,7 @@ photo_gallery = st.container()
 with distance_progress:
     st.header("Distance Progress")
     bst_date = pd.to_datetime("today").tz_localize("UTC").tz_convert("Europe/London").strftime("%d %B %Y %H:%M")
-    st.caption(f"Page last updated: {today_date}")
+    st.caption(f"Page data last updated: {today_date}")
 
     col1, col2 = st.columns([3,1], vertical_alignment="center")
 
@@ -139,7 +146,7 @@ with dist_calcs:
     dist_remaining = goal_distance - current_distance
 
     # Average distance needed per day
-    avg_daily_dist = dist_remaining / days_remaining
+    avg_daily_dist = dist_remaining / total_days_left
 
     # Average distance per participant
     avg_per_person = avg_daily_dist / number_participants
@@ -147,14 +154,14 @@ with dist_calcs:
     total_per_person = dist_remaining / number_participants
 
     metric1, metric2, metric3 = st.columns(3)
-    metric1.metric("Days Remaining", f"{days_remaining}")
+    metric1.metric("Time Remaining", f"{days_remaining} days {hours_remaining} hrs")
     metric2.metric("Distance Remaining", f"{dist_remaining} km")
     metric3.metric("Avg Distance per Day", f"{avg_daily_dist:.1f} km")
 
     with st.expander("Click to calculate distances per participant"):
         st.text(f"Number of Participants: {number_participants}")
         km1, km2 = st.columns(2)
-        km1.metric("Total Distance Needed per Person", f"{total_per_person:.1f} km")
+        km1.metric("Total Remaining Distance per Person", f"{total_per_person:.1f} km")
         km2.metric("Daily Distance Needed per Person", f"{avg_per_person:.1f} km")
 
         height_cm = st.number_input(
@@ -167,7 +174,7 @@ with dist_calcs:
 
         stride_length_m = height_cm * 0.415 / 100
         steps_remaining = total_per_person * 1000 / stride_length_m
-        avg_steps_per_day = steps_remaining / days_remaining
+        avg_steps_per_day = steps_remaining / total_days_left
 
         step1, step2 = st.columns(2)
         step1.metric("Total Steps Needed", f"{steps_remaining:,.0f}")
